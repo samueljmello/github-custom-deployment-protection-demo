55568b86fbdc9ba4f73871315a28ef50786423f4
Samuel Mello <6161359+samueljmello@users.noreply.github.com>
2023-09-06 13:29:45 -0400

    make a few fixes

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import * as crypto from "crypto";
 import bodyParser from 'body-parser';
 import { Octokit } from '@octokit/rest';
@@ -10,7 +10,9 @@ const WEBHOOK_SECRET: string = process.env.WEBHOOK_SECRET!;
 const GH_APP_ID: string = process.env.GH_APP_ID!;
 const GH_APP_INSTALL_ID: string = process.env.GH_APP_INSTALL_ID!;
 const GH_PRIVATE_KEY: string = process.env.GH_PRIVATE_KEY!;
+const PORT: number = 80
 
+// create octokit for communication to GitHub
 const octokit = new Octokit({
   authStrategy: createAppAuth,
   auth: {
@@ -20,10 +22,10 @@ const octokit = new Octokit({
   },
 });
 
-// set up app
+// set up express erver
 const app = express();
 
-// use body parser
+// use body parser for express
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -40,23 +42,31 @@ app.post('/', async (req, res) => {
     return;
   }
 
-  console.log(req.body);
+  // validate the type of request
+  if (req.body['deployment_callback_url'] === undefined) {
+    res.send("Skipping non-deployment webhook.");
+    return;
+  }
+
+  // make sure payload contains an environment name
+  if (req.body['deployment'] === undefined || req.body['deployment']['environment'] == undefined) {
+    return handle_error(res, "No deployment environment provided in payload.");
+  }
+
+  // logic for third party runs
 
   // send approval
-  // await octokit.request("POST /repos/{owner}/{repo}/actions/runs/{run_id}/deployment_protection_rule", {
-  //   owner: "mello-testing",
-  //   repo: "testing-deployment-protection",
-  //   run_id: 6097097112,
-  //   environment_name: "prod",
-  //   state: "approved",
-  //   comment: "LGTM"
-  // });
+  await octokit.request(`POST ${req.body['deployment_callback_url']}`, {
+    environment_name: req.body['deployment']['environment'],
+    state: "approved",
+    comment: "Passed external tests."
+  });
 
   res.send('OK');
 })
 
-app.listen(80, () => {
-    console.log('The application is listening on port 80!');
+app.listen(PORT, () => {
+    console.log(`The application is listening on port ${PORT}!`);
 })
 
 // verification method
@@ -69,3 +79,10 @@ const verify_signature = (req: Request) => {
   const untrusted = Buffer.from(`${req.headers["x-hub-signature-256"]}`, 'ascii');
   return crypto.timingSafeEqual(trusted, untrusted);
 };
+
+// quick handling of errors
+const handle_error = (res: Response, message: string) => {
+  console.log(message)
+  res.status(500);
+  res.send('500 Server Error');
+}
